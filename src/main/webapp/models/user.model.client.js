a4de051ae6281075ca2a83a4bd26433957c9caac
Ti Jiang <jiang.ti1@husky.neu.edu>
2018-09-20 11:32:41 -0400

    update user model js file

--- src/main/webapp/models/user.model.client.js
+++ src/main/webapp/models/user.model.client.js
@@ -3,6 +3,10 @@ function User(username, password, firstName, lastName) {
 	this.password = password;
 	this.firstName = firstName;
 	this.lastName = lastName;
+	this.email = "";
+	this.phone = "";
+	this.role = "";
+	this.dateOfBirth ="";
 
 	this.getUsername = getUsername;
 	this.setUsername = setUsername;
@@ -12,6 +16,14 @@ function User(username, password, firstName, lastName) {
 	this.setFirstName = setFirstName;
 	this.getLastName = getLastName;
 	this.setLastName = setLastName;
+	this.getEmail = getEmail;
+	this.setEmail = setEmail;
+	this.getPhone = getPhone;
+	this.setPhone = setPhone;
+	this.getRole = getRole;
+	this.setRole = setRole;
+	this.getDateOfBirth = getDateOfBirth;
+	this.setDateOfBirth = setDateOfBirth;
 
 	function getUsername() {
 		return this.username;
@@ -43,5 +55,37 @@ function User(username, password, firstName, lastName) {
 
 	function setLastName(lastName) {
 		this.lastName = lastName;
+	}
+
+	function getEmail() {
+		return this.email;
+	}
+
+	function setEmail(email) {
+		this.email = email;
+	}
+
+	function getPhone() {
+		return this.phone;
+	}
+
+	function setPhone(phone) {
+		this.phone = phone;
+	}
+
+	function getRole() {
+		return this.role;
+	}
+
+	function setRole(role) {
+		this.role = role;
+	}
+
+	function getDateOfBirth() {
+		return this.dateOfBirth;
+	}
+
+	function setDateOfBirth(dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
 	}
 }
