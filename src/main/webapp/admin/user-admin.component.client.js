69609f9743e3ddc68eafa44b3b72e7627609e332
Ti Jiang <jiang.ti1@husky.neu.edu>
2018-09-20 20:36:01 -0400

    add delete user and renderUsers and findAllUsers function

--- src/main/webapp/admin/user-admin.component.client.js
+++ src/main/webapp/admin/user-admin.component.client.js
@@ -5,6 +5,7 @@
     var $firstNameFld, $lastNameFld;
     var $userRowTemplate, $tbody;
     var $userRole;
+    var userTemplate;
     var userService = new AdminUserServiceClient();
     $(main);
 
@@ -18,33 +19,80 @@
         $createBtn = $("#createBtn");
         $updateBtn = $("#updateBtn");
 
+        userTemplate = $(".wbdv-template.wbdv-hidden").clone();
         $createBtn.click(createUser);
+        $updateBtn.click(updateUser);
+        renderUsers(userService.findAllUsers());
         
 
     }
     function createUser() {
-        
-        var newUser = $(".wbdv-template.wbdv-hidden").clone();
-        newUser.removeClass("wbdv-hidden");
-        newUser.find(".wbdv-username")
-               .html($usernameFld.val());
-        newUser.find(".wbdv-first-name")
-               .html($firstNameFld.val());
-        newUser.find(".wbdv-last-name")
-               .html($lastNameFld.val());
-        newUser.find(".wbdv-role")
-               .html($userRole.val());    
+        var timestamp = (new Date()).getTime();
+        var newUser = {
+            userID:timestamp,
+            username:$usernameFld.val(),
+            password:$passwordFld.val(),
+            firstName:$firstNameFld.val(),
+            lastName:$lastNameFld.val(),
+            role:$userRole.val(),
+        };
         $usernameFld.val("");
         $firstNameFld.val("");
         $lastNameFld.val("");
-        $userRole.val("");
-        $('.wbdv-tbody').append(newUser);
+        $passwordFld.val("");
+
+        userService.createUser(newUser);
+//        console.log(newUser)
+        renderUsers(userService.findAllUsers());
     }
+
+    function editUser(event) {
+
+    }
+
     function findAllUsers() {}
     function findUserById() {}
-    function deleteUser() {}
+    function deleteUser(event) {
+        var button = $(event.currentTarget);
+        var tr = button.parents(".wbdv-template");
+        var userId = tr.attr("id");
+        userService.deleteUser(userId);
+        tr.remove();
+        
+    }
+
     function selectUser() {}
     function updateUser() {}
-    function renderUser(user) {}
-    function renderUsers(users) {}
+
+    function renderUser(user) {
+        var newUser = $(".wbdv-template.wbdv-hidden").clone();
+        var timestamp = (new Date()).getTime();
+        newUser.removeClass("wbdv-hidden");
+        newUser.find(".wbdv-username").html(user.username);
+        newUser.find(".wbdv-first-name").html(user.firstName);
+        newUser.find(".wbdv-last-name").html(user.lastName);
+        newUser.find(".wbdv-role").html(user.role);
+        newUser.find(".wbdv-remove").attr("id", timestamp).click(deleteUser);
+        var timestamp2 = (new Date()).getTime();
+        newUser.find(".wbdv-edit").attr("id", timestamp2).click(editUser);
+        $('.wbdv-tbody').append(newUser);
+    }
+
+    function renderUsers(users) {
+        $(".wbdv-tbody").empty();
+        for (var i = 0; i < users.length; i++) {
+            var template = userTemplate.clone()
+            var user = users[i];
+            template.attr("id",user.userID);
+            template.removeClass("wbdv-hidden");
+            template.find(".wbdv-username").html(user.username);
+            template.find(".wbdv-first-name").html(user.firstName);
+            template.find(".wbdv-last-name").html(user.lastName);
+            template.find(".wbdv-role").html(user.role);
+            template.find(".wbdv-remove").click(deleteUser);
+            template.find(".wbdv-edit").click(editUser);
+            $('.wbdv-tbody').append(template);
+        }
+        
+    }
 })();
--- src/main/webapp/services/user.service.client.js
+++ src/main/webapp/services/user.service.client.js
@@ -1,3 +1,46 @@
+var allUsers = [
+            {
+                userID:1,
+                username:"Alice",
+                password:"12345",
+                firstName:"Alice",
+                lastName:"Bob",
+                role:"Student"
+            },
+            {
+                userID:2,
+                username:"JI",
+                password:"12345",
+                firstName:"JI",
+                lastName:"GO",
+                role:"Student"
+            },
+            {
+                userID:3,
+                username:"Bye",
+                password:"12345",
+                firstName:"Bye",
+                lastName:"Cook",
+                role:"Student"
+            },
+            {
+                userID:4,
+                username:"Cook",
+                password:"12345",
+                firstName:"Tim",
+                lastName:"Ushe",
+                role:"Faculty"
+            },
+            {
+                userID:5,
+                username:"Jane",
+                password:"12345",
+                firstName:"Jane",
+                lastName:"Oh",
+                role:"Student"
+            }
+        ];
+
 function AdminUserServiceClient() {
     this.createUser = createUser;
     this.findAllUsers = findAllUsers;
@@ -7,16 +50,16 @@ function AdminUserServiceClient() {
     /*this.url = 'http://localhost:8080/api/user';*/
     var self = this;
     function createUser(user, callback) {
-
+        allUsers.push(user);
     }
     function findAllUsers() {
-    	return [
-    		{username:'alice'},
-    		{username:'bob'},
-    		{username:'charly'}
-    	]
+       return allUsers;      
     }
     function findUserById(userId, callback) {}
     function updateUser(userId, user, callback) {}
-    function deleteUser(userId, callback) {}
+    function deleteUser(userId, callback) {
+        allUsers = $.grep(allUsers, function(user){
+            return user.userID != userId;
+        });
+    }
 }
